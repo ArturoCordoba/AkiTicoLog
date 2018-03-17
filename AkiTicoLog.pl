137a202dd6857ae909e17711a1377e8418561d85
Arturo Córdoba <arturocv16@gmail.com>
2018-03-17 00:03:26 -0600

    Creacion del archivo con la base de datos

---
+++ AkiTicoLog.pl
@@ -0,0 +1,12 @@
+%Base de datos de famositicos
+famositico("Keylor Navas", [[Edad, 31], [Genero, Hombre], [LugarDeNacimiento, "Perez Zeledon"], [Estatura, 1.85], [Deporte, Futbol], [Posicion, Portero], [Apodo, "La Pantera"], [Equipo, "Real Madrid"], [Dorsal, "1"], [ColorDeCabello, Negro]]).
+famositico("Oscar Ramirez", [[Edad, 53], [Genero, Hombre], [LugarDeNacimiento, "Heredia"], [Estatura, 1.68], [Deporte, Futbol], [Profesion, Tecnico], [Apodo, "El Machillo"], [ColorDeCabello, Negro]]).
+famositico("Hernan Medford", [[Edad, 49], [Genero, Hombre], [LugarDeNacimiento, "San Jose"], [Estatura, 1.76], [Deporte, Futbol], [Profesion, Tecnico], [Apodo, "Pelicano"], [Equipo, "Heredia"]]).
+famositico("Hanna Gabriel", [[Edad, 35], [Genero, Mujer], [LugarDeNacimiento, "Alajuela"], [Estatura, 1.87], [Deporte, Boxeo], [Profesion, Boxeadora], [ColorDeCabello, Negro]]).
+famositico("Andrey Amador", [[Edad, 31], [Genero, Hombre], [LugarDeNacimiento, "Alajuela"], [Estatura, 1.80], [Deporte, Ciclismo], [Profesion, Ciclista], [Equpo, "Movistar Team"], [ColorDeCabello, Negro]]).
+famositico("Shirley Cruz", [[Edad, 32], [Genero, Mujer], [LugarDeNacimiento, "San Jose"], [Estatura, 1.63], [Deporte, Futbol], [Profesion, Mediocampista], [ColorDeCabello, Negro], [Equipo, "Jiangsu Suning"]]).
+famositico("Mauricio Montero", [[Edad, 54], [Genero, Hombre], [LugarDeNacimiento, "Grecia"], [Estatura, 1.78], [Deporte, Futbol], [Profesion, Exfutbolista], [Equipo, "Liga Deportiva Alajuelense"], [ColorDeCabello, Negro],[Posicion, Defensa], [Apodo, "El Chunche"]]).
+famositico("Maria Jose Castillo", [[Edad, 27], [Genero, Mujer], [LugarDeNacimiento, "Heredia"], [Profesion, Cantante]]).
+famositico("Mario Chacon", [[Edad, 42], [Genero, Hombre], [LugarDeNacimiento, "San Jose"], [Profesion, Humorista], [Profesion, Actor]]).
+famositico("Celso Borges", [[Edad, 29], [Genero, Hombre], [LugarDeNacimiento, "San Jose"], [Estatura, 1.83], [Deporte, Futbol], [Posicion, Mediocentro], [Equipo, "Deportivo de la Coruña"], [Dorsal, 22], [ColorDeCabello, Negro], [Apodo, "La Maleta"]]).
+famositico("Bryan Ruiz", [[Edad, 32], [Genero, Hombre], [LugarDeNacimiento, "San Jose"], [Estatura, 1.88], [Deporte, Futbol], [Posicion, Mediapunta], [Equipo, "Sporting de Lisboa"], [Dorsal, 20], [Apodo, "El Capi"], [ColorDeCabello, Negro]]).
