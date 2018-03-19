e5d1193b16f8dd5739d9e2fb8c817e23bae8bf0f
Fagonar96 <fagonar96@gmail.com>
2018-03-18 23:44:44 -0600

    Creacion de archivo con base de datos v2

--- AkiTicoLog.pl
+++ AkiTicoLog.pl
@@ -4,9 +4,29 @@ famositico("Oscar Ramirez", [[Edad, 53], [Genero, Hombre], [LugarDeNacimiento, "
 famositico("Hernan Medford", [[Edad, 49], [Genero, Hombre], [LugarDeNacimiento, "San Jose"], [Estatura, 1.76], [Deporte, Futbol], [Profesion, Tecnico], [Apodo, "Pelicano"], [Equipo, "Heredia"]]).
 famositico("Hanna Gabriel", [[Edad, 35], [Genero, Mujer], [LugarDeNacimiento, "Alajuela"], [Estatura, 1.87], [Deporte, Boxeo], [Profesion, Boxeadora], [ColorDeCabello, Negro]]).
 famositico("Andrey Amador", [[Edad, 31], [Genero, Hombre], [LugarDeNacimiento, "Alajuela"], [Estatura, 1.80], [Deporte, Ciclismo], [Profesion, Ciclista], [Equpo, "Movistar Team"], [ColorDeCabello, Negro]]).
-famositico("Shirley Cruz", [[Edad, 32], [Genero, Mujer], [LugarDeNacimiento, "San Jose"], [Estatura, 1.63], [Deporte, Futbol], [Profesion, Mediocampista], [ColorDeCabello, Negro], [Equipo, "Jiangsu Suning"]]).
+famositico("Shirley Cruz", [[Edad, 32], [Genero, Mujer], [LugarDeNacimiento, "San Jose"], [Estatura, 1.63], [Deporte, Futbol], [Posicion, Mediocampista], [ColorDeCabello, Negro], [Equipo, "Jiangsu Suning"]]).
 famositico("Mauricio Montero", [[Edad, 54], [Genero, Hombre], [LugarDeNacimiento, "Grecia"], [Estatura, 1.78], [Deporte, Futbol], [Profesion, Exfutbolista], [Equipo, "Liga Deportiva Alajuelense"], [ColorDeCabello, Negro],[Posicion, Defensa], [Apodo, "El Chunche"]]).
 famositico("Maria Jose Castillo", [[Edad, 27], [Genero, Mujer], [LugarDeNacimiento, "Heredia"], [Profesion, Cantante]]).
 famositico("Mario Chacon", [[Edad, 42], [Genero, Hombre], [LugarDeNacimiento, "San Jose"], [Profesion, Humorista], [Profesion, Actor]]).
 famositico("Celso Borges", [[Edad, 29], [Genero, Hombre], [LugarDeNacimiento, "San Jose"], [Estatura, 1.83], [Deporte, Futbol], [Posicion, Mediocentro], [Equipo, "Deportivo de la Coruña"], [Dorsal, 22], [ColorDeCabello, Negro], [Apodo, "La Maleta"]]).
 famositico("Bryan Ruiz", [[Edad, 32], [Genero, Hombre], [LugarDeNacimiento, "San Jose"], [Estatura, 1.88], [Deporte, Futbol], [Posicion, Mediapunta], [Equipo, "Sporting de Lisboa"], [Dorsal, 20], [Apodo, "El Capi"], [ColorDeCabello, Negro]]).
+famositico("Joel Campbell", [[Edad, 25], [Genero, Hombre], [LugarDeNacimiento, "San Jose"], [Estatura, 1.78], [Deporte, Futbol], [Posicion, Volante Delantero], [Equipo, "Real Betis"], [Dorsal, 12], [ColorDeCabello, Negro]]).
+famositico("Debi Nova", [[Edad, 37], [Genero, Mujer], [LugarDeNacimiento, "San Jose"], [Profesion, Cantante]]).
+famositico("Ignacio Santos", [[Edad, 56], [Genero, Hombre], [LugarDeNacimiento, "Cuba"], [Profesion, Periodista], [Profesion, Abogado]]).
+famositico("Pilar Cisneros", [[Edad, 63], [Genero, Mujer], [LugarDeNacimiento, "Peru"], [Profesion, Periodista]]).
+famositico("Leonora Jimenez", [[Edad, 35], [Genero, Mujer], [LugarDeNacimiento, "San Jose"], [Profesion, Modelo]]).
+famositico("Maribel Guardia", [[Edad, 58], [Genero, Mujer], [LugarDeNacimiento, "San Jose"], [Profesion, Modelo], [Profesion, Actriz], [Profesion, Cantante]]).
+famositico("Francisca Carrasco", [[Edad, 64], [Genero, Mujer], [LugarDeNacimiento, "Cartago"], [LugarDeFallecimiento, "San Jose"], [Profesion, Militar]]).
+famositico("Franklin Chang", [[Edad, 67],[Genero, Hombre], [LugarDeNacimiento, "San Jose"], [Profesion, ExAstronauta],  [Profesion, Fisico]]).
+famositico("Clodomiro Picado" [[Edad, 57], [Genero, Hombre], [LugarDeNacimiento, "Nicaragua"], [LugarDeFallecimiento, "San Jose"], [Profesion, Cientifico], [Apodo, "Clorito Picado"]]).
+famositico("Claudia Poll", [[Edad, 43], [Genero, Mujer], [LugarDeNacimiento, "Nicaragua"], [Estatura, 1.91], [Deporte, Natacion], [Profesion, Nadadora]]).
+famositico("Sylvia Poll", [[Edad, 45], [Genero, Mujer], [LugarDeNacimiento, "Nicaragua"], [Deporte, Natacion], [Profesion, Nadadora], [Profesion, Diplomatica]]).
+famositico("Erick Lonnis", [[Edad, 52], [Genero, Hombre], [LugarDeNacimiento, "San Jose"], [Estatura, 1.82], [Deporte, Futbol], [Posicion, Portero], [Profesion, Exfutbolista], [Profesion, Comentarista], [Apodo, "La Anguila"]]).
+famositico("Manuel Salazar", [[Edad, 63], [Genero, Hombre], [LugarDeNacimiento, "San Jose"], [LugarDeFallecimiento, "San Jose"], [Profesion, Cantante], [Apodo, "Melico Salazar"]]).
+famositico("Oscar Arias", [[Edad, 77], [Genero, Hombre], [LugarDeNacimiento, "San Jose"], [Profesion, Expresidente], [Profesion, Abogado], [Profesion, Politologo]]).
+famositico("Hernan Jimenez", [[Edad, 37], [Genero, Hombre], [LugarDeNacimiento, "San Jose"], [Profesion, Comediante], [Profesion, Actor], [Profesion, Director Cinematografico]]).
+famositico("Mario McGregor", [[Edad, 59], [Genero, Hombre], [LugarDeNacimiento, "Limon"], [LugarDeFallecimiento, "San Jose"], [Profesion, Narrador], [Apodo, "Doble M"]]).
+famositico("Juan Santamaria", [[Edad, 25], [Genero, Hombre], [LugarDeNacimiento, "Alajuela"], [LugarDeFallecimiento, "Nicaragua"], [Profesion, Soldado], [Apodo, "El Tamborcillo"]]).
+famositico("Everardo Herrera", [[Edad, 56], [Genero, Hombre], [LugarDeNacimiento, "San Jose"], [Profesion, Comentarista], [Profesion, Periodista]]).
+
+
