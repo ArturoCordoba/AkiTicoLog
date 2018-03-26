c5d0b28463f07e4a758ee1674f720a2ee8c54504
Fagonar96 <fagonar96@gmail.com>
2018-03-25 19:58:43 -0600

    Base de datos de famositicos v3

---
+++ Database.pl
@@ -0,0 +1,59 @@
+%Base de datos de famositicos
+
+% famositico(Nombre, Genero, Edad,LugarDeNacimiento, LugarDeFallecimiento, Profesion, Estatura, ColorDeCabello, Apodo, Posicion, Equipo)
+famositico("Keylor Navas", hombre, 31, "San Jose",_, futbolista, 1.85, negro, "La Pantera", portero, "Real Madrid").
+famositico("Celso Borges", hombre, 29, "San Jose",_, futbolista, 1.83, negro, "La Maleta", mediocampista, "Deportivo la Coruña").
+famositico("Bryan Ruiz", hombre, 32, "San Jose",_, futbolista, 1.88, negro, "El Capi", delantero, "Sporting de Lisboa").
+famositico("Shirley Cruz", mujer, 32, "San Jose",_, futbolista, 1.63, negro,_, mediocampista, "Jiangsu Suning").
+famositico("Joel Campbell", hombre, 25, "San Jose",_, futbolista, 1.78, negro,_, delantero, "Real Betis").
+famositico("Cristian Gamboa", hombre, 28, "Guanacaste",_, futbolista, 1.73, negro,_,_,defensa,"Celtic").
+famositico("Kendall Waston", hombre, 30, "San Jose",_, futbolista, 1.96, negro,"El Gigante", defensa, "Vancouver Whitecaps FC").
+
+famositico("Alonso Solis", hombre, 39, "San Jose",_, exfutbolista, 1.78, negro, "El Mariachi", delantero, "Deportivo Saprissa").
+famositico("Mauricio Montero", hombre, 54, "Alajuela",_, exfutbolista, 1.78, negro, "El Chunche", defensa, "Liga Deportiva Alajuelense").
+famositico("Erick Lonnis", hombre, 52, "San Jose",_, exfutbolista, 1.82, calvo, "La Anguila", portero, "Deportivo Saprissa").
+famositico("Rolando Fonseca", hombre, 43, "San Jose",_, exfutbolista, 1.72, negro, "El Principito", delantero, "Deportivo Saprissa").
+famositico("Paulo Wanchope", hombre, 41, "Heredia",_, exfutbolista, 1.93, calvo, "Chope", delantero, "Club Sport Herediano").
+famositico("Wilmer Lopez", hombre, 46, "Alajuela",_, exfutbolista, 1.69, negro, "Pato", mediocampista, "Liga Deportiva Alajuelensa").
+
+famositico("Walter Centeno", hombre, 43, "Puntarenas",_, tecnico, 1.74, negro, "Pate Centeno",_,"Municipal de Grecia").
+famositico("Oscar Ramirez", hombre, 53, "Heredia",_, tecnico, 1.68, negro, "El Machillo",_,"Seleccion Nacional Costa Rica").
+famositico("Hernan Medford", hombre, 49, "San Jose",_, tecnico, 1.76, calvo, "El Pelicano",_,"C.S.D. Municipal").
+
+famositico("Hanna Gabriel", mujer, 35, "Alajuela",_, boxeadora, 1.87, negro, "La Amazona",_,_).
+famositico("Andrey Amador", hombre, 31, "Alajuela",_, ciclista, 1.80, negro,_,_,"Movistar Team").
+famositico("Claudia Poll", mujer, 43, "Nicaragua",_, nadadora, 1.91, rubio,_,_,_).
+famositico("Sylvia Poll", mujer, 45, "Nicaragua",_, nadadora, _, rubio,_,_,_).
+famositico("Nery Brenes", hombre, 32, "Limon",_, atleta, 1.75, negro,_,_,_).
+
+famositico("Maria Jose Castillo", mujer, 27, "Heredia",_, cantante,_, negro,_,_,_).
+famositico("Deborah Nowalski", mujer, 37, "San Jose",_, cantante,_, rubio, "Debi Nova",_,_).
+famositico("Manuel Salazar", hombre, 63, "San Jose", "San Jose", cantante,_, negro, "Melico Salazar",_,_).
+famositico("Jose Capmany", hombre, 40, "San Jose", "San Jose", cantante,_, negro,_,_,_).
+famositico("Fidel Gamboa", hombre, 50, "Guanacaste", "Heredia", cantante,_, negro,_,_,_).
+
+famositico("Mario Chacon", hombre, 42, "San Jose",_, comediante,_, negro,"Maikol Yordan",_,_).
+famositico("Hernan Jimenez", hombre, 37, "San Jose",_, comediante,_, negro,_,_,_).
+famositico("Carlos Ramos", hombre, 68, "San Jose",_, comediante,_, negro, "El Porcionzon",_,_).
+
+famositico("Ignacio Santos", hombre, 56, "Cuba",_, periodista,_, negro,_,_,_).
+famositico("Pilar Cisneros", mujer, 63, "Peru",_, periodista,_, negro,_,_,_).
+
+famositico("Everardo Herrera", hombre, 56, "San Jose",_, comentarista,_, blanco,_,_,_).
+famositico("Mario McGregor", hombre, 59, "Limon", "San Jose", narrador,_, negro, "Doble M",_,_).
+
+famositico("Leonora Jimenez", mujer, 35, "San Jose",_, modelo,_, rubio,_,_,_).
+famositico("Maribel Guardia", mujer, 58, "San Jose",_, actriz,_, negro,_,_,_).
+
+famositico("Franklin Chang", hombre, 67, "San Jose",_, exaustronauta,_, negro,_,_,_).
+famositico("Clodomiro Picado", hombre, 57, "Nicaragua", "San Jose", cientifico,_, negro, "Clorito Picado",_,_).
+
+famositico("Oscar Arias", hombre, 77, "San Jose",_, expresidente,_,gris,_,_,_).
+
+famositico("Francisca Carrasco", mujer,64,"Cartago","San Jose", militar,_, negro, "Pancha Carrasco",_,_).
+famositico("Juan Santamaria", hombre, 25, "Alajuela", "Nicaragua", militar,_,negro,"El Tamborcillo",_,_).
+
+
+
+
+
