37170ab245d0d3a32b2f87805e3ac8432d974016
Fagonar96 <fagonar96@gmail.com>
2018-03-27 16:53:59 -0600

    Base de datos de famositicos v4

--- Database.pl
+++ Database.pl
@@ -1,57 +1,57 @@
 %Base de datos de famositicos
 
 % famositico(Nombre, Genero, Edad,LugarDeNacimiento, LugarDeFallecimiento, Profesion, Estatura, ColorDeCabello, Apodo, Posicion, Equipo)
-famositico("Keylor Navas", hombre, 31, "San Jose",_, futbolista, 1.85, negro, "La Pantera", portero, "Real Madrid").
-famositico("Celso Borges", hombre, 29, "San Jose",_, futbolista, 1.83, negro, "La Maleta", mediocampista, "Deportivo la Coruña").
-famositico("Bryan Ruiz", hombre, 32, "San Jose",_, futbolista, 1.88, negro, "El Capi", delantero, "Sporting de Lisboa").
-famositico("Shirley Cruz", mujer, 32, "San Jose",_, futbolista, 1.63, negro,_, mediocampista, "Jiangsu Suning").
-famositico("Joel Campbell", hombre, 25, "San Jose",_, futbolista, 1.78, negro,_, delantero, "Real Betis").
-famositico("Cristian Gamboa", hombre, 28, "Guanacaste",_, futbolista, 1.73, negro,_,_,defensa,"Celtic").
-famositico("Kendall Waston", hombre, 30, "San Jose",_, futbolista, 1.96, negro,"El Gigante", defensa, "Vancouver Whitecaps FC").
+famositico("Keylor Navas", "hombre", "31", "san jose",_, "futbolista", "1.85", "negro", "pantera", "portero", "real madrid").
+famositico("Celso Borges", "hombre", "29", "san jose",_, "futbolista", "1.83", "negro", "maleta", "mediocampista", "deportivo coruña").
+famositico("Bryan Ruiz", "hombre", "32", "san jose",_, "futbolista", "1.88", "negro", "capi", "delantero", "sporting de lisboa").
+famositico("Shirley Cruz", "mujer", "32", "san jose",_, "futbolista", "1.63", "negro",_, "mediocampista", "jiangsu suning").
+famositico("Joel Campbell", "hombre", "25", "san jose",_, "futbolista", "1.78", "negro",_, "delantero", "betis").
+famositico("Cristian Gamboa", "hombre", "28", "guanacaste",_, "futbolista", "1.73", "negro",_,_,"defensa","celtic").
+famositico("Kendall Waston", "hombre", "30", "san jose",_, "futbolista", "1.96", "negro", "gigante", "defensa", "vancouver whitecaps").
 
-famositico("Alonso Solis", hombre, 39, "San Jose",_, exfutbolista, 1.78, negro, "El Mariachi", delantero, "Deportivo Saprissa").
-famositico("Mauricio Montero", hombre, 54, "Alajuela",_, exfutbolista, 1.78, negro, "El Chunche", defensa, "Liga Deportiva Alajuelense").
-famositico("Erick Lonnis", hombre, 52, "San Jose",_, exfutbolista, 1.82, calvo, "La Anguila", portero, "Deportivo Saprissa").
-famositico("Rolando Fonseca", hombre, 43, "San Jose",_, exfutbolista, 1.72, negro, "El Principito", delantero, "Deportivo Saprissa").
-famositico("Paulo Wanchope", hombre, 41, "Heredia",_, exfutbolista, 1.93, calvo, "Chope", delantero, "Club Sport Herediano").
-famositico("Wilmer Lopez", hombre, 46, "Alajuela",_, exfutbolista, 1.69, negro, "Pato", mediocampista, "Liga Deportiva Alajuelensa").
+famositico("Alonso Solis", "hombre", "39", "san jose",_, "exfutbolista", "1.78", "negro", "mariachi", "delantero", "saprissa").
+famositico("Mauricio Montero", "hombre", "54", "alajuela",_, "exfutbolista", "1.78", "negro", "chunche", "defensa", "liga deportiva alajuelense").
+famositico("Erick Lonnis", "hombre", "52", "san Jose",_, "exfutbolista", "1.82", "calvo", "anguila", "portero", "saprissa").
+famositico("Rolando Fonseca", "hombre", "43", "san Jose",_, "exfutbolista", "1.72", "negro", "principito", "delantero", "saprissa").
+famositico("Paulo Wanchope", "hombre", "41", "heredia",_, "exfutbolista", "1.93", "calvo", "chope", "delantero", "club sport herediano").
+famositico("Wilmer Lopez", "hombre", "46", "alajuela",_, "exfutbolista", "1.69", "negro", "pato", "mediocampista", "liga deportiva alajuelensa").
 
-famositico("Walter Centeno", hombre, 43, "Puntarenas",_, tecnico, 1.74, negro, "Pate Centeno",_,"Municipal de Grecia").
-famositico("Oscar Ramirez", hombre, 53, "Heredia",_, tecnico, 1.68, negro, "El Machillo",_,"Seleccion Nacional Costa Rica").
-famositico("Hernan Medford", hombre, 49, "San Jose",_, tecnico, 1.76, calvo, "El Pelicano",_,"C.S.D. Municipal").
+famositico("Walter Centeno", "hombre", "43", "puntarenas",_, "tecnico", "1.74", "negro", "pate", "mediocampista","municipal grecia").
+famositico("Oscar Ramirez", "hombre", "53", "heredia",_, "tecnico", "1.68", "negro", "machillo","mediocampista","seleccion costa rica").
+famositico("Hernan Medford", "hombre", "49", "san jose",_, "tecnico", "1.76", "calvo", "pelicano","delantero","csd municipal").
 
-famositico("Hanna Gabriel", mujer, 35, "Alajuela",_, boxeadora, 1.87, negro, "La Amazona",_,_).
-famositico("Andrey Amador", hombre, 31, "Alajuela",_, ciclista, 1.80, negro,_,_,"Movistar Team").
-famositico("Claudia Poll", mujer, 43, "Nicaragua",_, nadadora, 1.91, rubio,_,_,_).
-famositico("Sylvia Poll", mujer, 45, "Nicaragua",_, nadadora, _, rubio,_,_,_).
-famositico("Nery Brenes", hombre, 32, "Limon",_, atleta, 1.75, negro,_,_,_).
+famositico("Hanna Gabriel", "mujer", "35", "alajuela",_, "boxeadora", "1.87", "negro", "amazona",_,_).
+famositico("Andrey Amador", "hombre", "31", "alajuela",_, "ciclista", "1.80", "negro",_,_,"movistar team").
+famositico("Claudia Poll", "mujer", "43", "nicaragua",_, "nadadora", "1.91", "rubio",_,_,_).
+famositico("Sylvia Poll", "mujer", "45", "nicaragua",_, "nadadora", _, "rubio",_,_,_).
+famositico("Nery Brenes", "hombre", "32", "limon",_, "atleta", "1.75", "negro",_,_,_).
 
-famositico("Maria Jose Castillo", mujer, 27, "Heredia",_, cantante,_, negro,_,_,_).
-famositico("Deborah Nowalski", mujer, 37, "San Jose",_, cantante,_, rubio, "Debi Nova",_,_).
-famositico("Manuel Salazar", hombre, 63, "San Jose", "San Jose", cantante,_, negro, "Melico Salazar",_,_).
-famositico("Jose Capmany", hombre, 40, "San Jose", "San Jose", cantante,_, negro,_,_,_).
-famositico("Fidel Gamboa", hombre, 50, "Guanacaste", "Heredia", cantante,_, negro,_,_,_).
+famositico("Maria Jose Castillo", "mujer", "27", "heredia",_, "cantante",_, "negro",_,_,_).
+famositico("Deborah Nowalski", "mujer", "37", "san jose",_, "cantante",_, "rubio", "debi nova",_,_).
+famositico("Manuel Salazar", "hombre", "63", "san jose", "san jose", "cantante",_, "negro", "melico",_,_).
+famositico("Jose Capmany", "hombre", "40", "san jose", "san jose", "cantante",_, "negro",_,_,_).
+famositico("Fidel Gamboa", "hombre", "50", "guanacaste", "heredia", "cantante",_, "negro",_,_,_).
 
-famositico("Mario Chacon", hombre, 42, "San Jose",_, comediante,_, negro,"Maikol Yordan",_,_).
-famositico("Hernan Jimenez", hombre, 37, "San Jose",_, comediante,_, negro,_,_,_).
-famositico("Carlos Ramos", hombre, 68, "San Jose",_, comediante,_, negro, "El Porcionzon",_,_).
+famositico("Mario Chacon", "hombre", "42", "san jose",_, "comediante",_, "negro","maikol yordan",_,_).
+famositico("Hernan Jimenez", "hombre", "37", "san jose",_, "comediante",_, "negro",_,_,_).
+famositico("Carlos Ramos", "hombre", "68", "san jose",_, "comediante",_, "negro", "porcionzon",_,_).
 
-famositico("Ignacio Santos", hombre, 56, "Cuba",_, periodista,_, negro,_,_,_).
-famositico("Pilar Cisneros", mujer, 63, "Peru",_, periodista,_, negro,_,_,_).
+famositico("Ignacio Santos", "hombre", "56", "cuba",_, "periodista",_, "negro",_,_,_).
+famositico("Pilar Cisneros", "mujer", "63", "peru",_, "periodista",_, "negro",_,_,_).
 
-famositico("Everardo Herrera", hombre, 56, "San Jose",_, comentarista,_, blanco,_,_,_).
-famositico("Mario McGregor", hombre, 59, "Limon", "San Jose", narrador,_, negro, "Doble M",_,_).
+famositico("Everardo Herrera", "hombre", "56", "san jose",_, "comentarista",_, "blanco",_,_,_).
+famositico("Mario McGregor", "hombre", "59", "limon", "san jose", "narrador",_, "negro", "doble m",_,_).
 
-famositico("Leonora Jimenez", mujer, 35, "San Jose",_, modelo,_, rubio,_,_,_).
-famositico("Maribel Guardia", mujer, 58, "San Jose",_, actriz,_, negro,_,_,_).
+famositico("Leonora Jimenez", "mujer", "35", "san jose",_, "modelo",_, "rubio",_,_,_).
+famositico("Maribel Guardia", "mujer", "58", "san jose",_, "actriz",_, "negro",_,_,_).
 
-famositico("Franklin Chang", hombre, 67, "San Jose",_, exaustronauta,_, negro,_,_,_).
-famositico("Clodomiro Picado", hombre, 57, "Nicaragua", "San Jose", cientifico,_, negro, "Clorito Picado",_,_).
+famositico("Franklin Chang", "hombre", "67", "san jose",_, "exaustronauta",_, "negro",_,_,_).
+famositico("Clodomiro Picado", "hombre", "57", "nicaragua", "san jose", "cientifico",_, "negro", "clorito",_,_).
 
-famositico("Oscar Arias", hombre, 77, "San Jose",_, expresidente,_,gris,_,_,_).
+famositico("Oscar Arias", "hombre", "77", "san jose",_, "expresidente",_,"gris",_,_,_).
 
-famositico("Francisca Carrasco", mujer,64,"Cartago","San Jose", militar,_, negro, "Pancha Carrasco",_,_).
-famositico("Juan Santamaria", hombre, 25, "Alajuela", "Nicaragua", militar,_,negro,"El Tamborcillo",_,_).
+famositico("Francisca Carrasco", "mujer", "64","cartago","san jose", "militar",_, "negro", "pancha",_,_).
+famositico("Juan Santamaria", "hombre", "25", "alajuela", "nicaragua", "militar",_, "negro","tamborcillo",_,_).
 
 
 
