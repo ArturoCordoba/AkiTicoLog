76fa7453fa112bfed7979a1dd79619d56778b0c7
Arturo Córdoba <arturocv16@gmail.com>
2018-03-27 17:00:59 -0600

    analizador v4
    Adición de las correcciones a la hora de leer una respuesta que está compuesta por dos palabras, por ejemplo: "San Jose"

--- analizador.pl
+++ analizador.pl
@@ -40,3 +40,22 @@ verbo_identificado(X, Z):-primer_elemento(X,Z).
 
 %pregunta():-write("Cual es su nombre?"),nl,read_line_to_codes(user_input, Codes), atom_codes(A, Codes), atom_string(A, String), split_string(String," ","",List),ultimo_elemento_lista(List,Last),eliminar_caracter_string(Last,".",NewLast),eliminar_elemento_lista(Last,List,NewList),convertir_string_lista(NewLast,LastList),append(NewList,LastList,NEWList),analizador(NEWList,Z),write(Z),!.
 
+corregir(X,Y):-igualdad(X,"San"),igualdad(Y,"San Jose"),!.
+corregir(X,Y):-igualdad(X,"Costa"),igualdad(Y,"Costa Rica"),!.
+corregir(X,Y):-igualdad(X,"Real"),igualdad(Y,"Real Madrid"),!.
+corregir(X,Y):-igualdad(X,"Deportivo"),igualdad(Y,"Deportivo La Coruña"),!.
+corregir(X,Y):-igualdad(X,"Sporting"),igualdad(Y,"Sporting de Lisboa"),!.
+corregir(X,Y):-igualdad(X,"Jiangsu"),igualdad(Y,"Jiangsu Suning"),!.
+corregir(X,Y):-igualdad(X,"Vancouver"),igualdad(Y,"Vancouver Whitecaps FC"),!.
+corregir(X,Y):-igualdad(X,"Liga"),igualdad(Y,"Liga Deportiva Alajuelense"),!.
+corregir(X,Y):-igualdad(X,"Club"),igualdad(Y,"Club Sport Herediano"),!.
+corregir(X,Y):-igualdad(X,"Municipal"),igualdad(Y,"Municipal de Grecia"),!.
+corregir(X,Y):-igualdad(X,"Seleccion"),igualdad(Y,"Seleccion Nacional Costa Rica"),!.
+corregir(X,Y):-igualdad(X,"CSD"),igualdad(Y,"CSD Municipal"),!.
+corregir(X,Y):-igualdad(X,"Movistar"),igualdad(Y,"Movistar Team"),!.
+corregir(X,Y):-igualdad(X,"Debi"),igualdad(Y,"Debi Nova"),!.
+corregir(X,Y):-igualdad(X,"Melico"),igualdad(Y,"Melico Salazar"),!.
+corregir(X,Y):-igualdad(X,"Maikol"),igualdad(Y,"Maikol Yordan"),!.
+corregir(X,Y):-igualdad(X,"Doble"),igualdad(Y,"Doble M"),!.
+corregir(X,Y):-igualdad(X,"Clorito"),igualdad(Y,"Clorito Picado"),!.
+corregir(X,Y):-igualdad(X,"Pancha"),igualdad(Y,"Pancha Carrasco"),!.
