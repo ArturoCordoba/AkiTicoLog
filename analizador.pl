ec90dddb0bac3b9b840f7b7c2d3e4797fcac2780
Arturo Córdoba <arturocv16@gmail.com>
2018-03-27 16:44:43 -0600

    analizador v3

--- analizador.pl
+++ analizador.pl
@@ -35,7 +35,7 @@ analizador([_|Y],Z):-analizador(Y,Z).
 analizador([], Z):-igualdad("no entendi", Z),!.
 
 %Obtener la respuesta del usuario
-verbo_identificado([X|Y], Z):-determinante(X),primer_elemento(Y,Z),!.
+verbo_identificado([X|Y], Z):-determinante(X),verbo_identificado(Y, Z),!.
 verbo_identificado(X, Z):-primer_elemento(X,Z).
 
 %pregunta():-write("Cual es su nombre?"),nl,read_line_to_codes(user_input, Codes), atom_codes(A, Codes), atom_string(A, String), split_string(String," ","",List),ultimo_elemento_lista(List,Last),eliminar_caracter_string(Last,".",NewLast),eliminar_elemento_lista(Last,List,NewList),convertir_string_lista(NewLast,LastList),append(NewList,LastList,NEWList),analizador(NEWList,Z),write(Z),!.
